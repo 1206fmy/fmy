bb9eaad5c22a115bb5ac796f77fb1fd548e51735
fz303 <980637515@qq.com>
2018-03-17 22:18:05 +0800

    新增文件

---
+++ luckfaimly/src/task_svr/lf_task_service_handler.cpp
@@ -0,0 +1,20 @@
+#include"task_svr/lf_task_service_handler.h"
+namespace LF
+{
+	bool lf_task_service_handler::login(const lf_string & name, const lf_string & password, lf_string & out_json)
+	{
+		return false;
+	}
+	bool lf_task_service_handler::logout(const lf_string & uid, lf_string & out_json)
+	{
+		return false;
+	}
+	bool lf_task_service_handler::user_register(const lf_string & param, lf_string & out_json)
+	{
+		return false;
+	}
+	bool lf_task_service_handler::register_confirm(const lf_string & param, lf_string & out_json)
+	{
+		return false;
+	}
+}
